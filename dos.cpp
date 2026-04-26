0fd2397e85d5fbeae17b7df1b22c5b4757689c0f
Franco <jloemig@gmail.com>
2026-04-26 18:22:58 -0300

    Correccion de sintaxis

--- dos.cpp
+++ dos.cpp
@@ -1,6 +1,6 @@
 // Git & GitHub in C++ Program
 #include <iostream>
 int main() {
-    std::cout << "This is the "dos.cpp" file. Hello World!";
+    std::cout << "This is the dos.cpp file. Hello World!";
     return 0;
 }
